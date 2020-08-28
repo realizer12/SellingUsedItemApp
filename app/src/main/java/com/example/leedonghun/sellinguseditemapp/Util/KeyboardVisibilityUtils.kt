d7f75ecf210959209221584ce3f99bd506ac6f84
LeeDonghun <dlwldlwldus123@gmail.com>
2020-08-28 22:40:38 +0900

    키보드 visible 관련 지원하는 uilt 클래스  default 생성자  추가ㅏ

--- app/src/main/java/com/example/leedonghun/sellinguseditemapp/Util/KeyboardVisibilityUtils.kt
+++ app/src/main/java/com/example/leedonghun/sellinguseditemapp/Util/KeyboardVisibilityUtils.kt
@@ -19,10 +19,15 @@ import android.view.inputmethod.InputMethodManager
  * 이부분  다시 한 번  공부하기
  */
 class KeyboardVisibilityUtils(
-
-    private val window: Window,
+    private val window: Window?,
     private val onShowKeyboard: ((keyboardHeight: Int, visibleDisplayFrameHeight: Int) -> Unit)? = null,
-    private val onHideKeyboard: (() -> Unit)? = null) {
+    private val onHideKeyboard: (() -> Unit)? = null)  {
+
+    //그냥 empty 생성자
+    constructor() : this(null,null,null)
+
+    lateinit var context:Context
+
 
 
     private val MIN_KEYBOARD_HEIGHT_PX = 150
@@ -32,7 +37,7 @@ class KeyboardVisibilityUtils(
 
 
     private val onGlobalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
-        window.decorView.getWindowVisibleDisplayFrame(windowVisibleDisplayFrame)
+        window?.decorView?.getWindowVisibleDisplayFrame(windowVisibleDisplayFrame)
 
         val visibleDecorViewHeight = windowVisibleDisplayFrame.height()
 
@@ -42,10 +47,12 @@ class KeyboardVisibilityUtils(
             if (lastVisibleDecorViewHeight > visibleDecorViewHeight + MIN_KEYBOARD_HEIGHT_PX) {
 
                 //Calculate current keyboard height (this includes also navigation bar height when in fullscreen mode).
-                val currentKeyboardHeight = window.decorView.height - windowVisibleDisplayFrame.bottom
+                val currentKeyboardHeight = window?.decorView?.height?.minus(
+                    windowVisibleDisplayFrame.bottom
+                )
 
                 //Notify listener about keyboard being shown.
-                onShowKeyboard?.invoke(currentKeyboardHeight, windowVisibleDisplayFrame.height())
+                currentKeyboardHeight?.let { onShowKeyboard?.invoke(it, windowVisibleDisplayFrame.height()) }
 
             }else if(lastVisibleDecorViewHeight + MIN_KEYBOARD_HEIGHT_PX < visibleDecorViewHeight){
 
@@ -59,18 +66,19 @@ class KeyboardVisibilityUtils(
     }
 
     init{
-        window.decorView.viewTreeObserver.addOnGlobalLayoutListener(onGlobalLayoutListener)
+        window?.decorView?.viewTreeObserver?.addOnGlobalLayoutListener(onGlobalLayoutListener)
     }
 
 
     fun detachKeyboardListeners() {
-        window.decorView.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalLayoutListener)
+        window?.decorView?.viewTreeObserver?.removeOnGlobalLayoutListener(onGlobalLayoutListener)
     }
 
 
 
     //키보드 숨기기
     fun hideKeyboard(context: Context,view: View) {
+        Logger.v("키보드 숨기기  실행")
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }

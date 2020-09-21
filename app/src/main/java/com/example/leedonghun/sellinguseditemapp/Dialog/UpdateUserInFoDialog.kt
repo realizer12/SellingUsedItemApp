e279d51bd78cde600dbc3d927b528239c1022003
LeeDonghun <dlwldlwldus123@gmail.com>
2020-09-21 22:29:10 +0900

    프로필 이미지 수정 옵션 다이얼로그  기능 추가 및  이미지  편집 option에 따른  변수 값 변경처리 완료

--- app/src/main/java/com/example/leedonghun/sellinguseditemapp/Dialog/UpdateUserInFoDialog.kt
+++ app/src/main/java/com/example/leedonghun/sellinguseditemapp/Dialog/UpdateUserInFoDialog.kt
@@ -2,21 +2,29 @@ package com.example.leedonghun.sellinguseditemapp.Dialog
 
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.R.drawable
 import android.app.AlertDialog
 import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
-import android.text.Layout
 import android.view.*
 import android.widget.Toast
 import androidx.constraintlayout.widget.Group
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import com.bumptech.glide.Glide
 import com.example.leedonghun.sellinguseditemapp.R
-import com.example.leedonghun.sellinguseditemapp.Util.*
+import com.example.leedonghun.sellinguseditemapp.Util.GetWindowSize
+import com.example.leedonghun.sellinguseditemapp.Util.KeyboardVisibilityUtils
+import com.example.leedonghun.sellinguseditemapp.Util.Logger
+import com.example.leedonghun.sellinguseditemapp.Util.PermissionCheck
+import gun0912.tedimagepicker.builder.TedImagePicker
+import gun0912.tedimagepicker.builder.type.MediaType
 import kotlinx.android.synthetic.main.custom_profile_img_edit_dialog.view.*
 import kotlinx.android.synthetic.main.update_user_info_dialog.*
 import kotlinx.android.synthetic.main.update_user_info_dialog.view.*
@@ -38,15 +46,22 @@ class UpdateUserInFoDialog(
 ):DialogFragment()
 {
 
-    lateinit var editProfileImageDialog: AlertDialog
-    lateinit var editProfileImageDialogBuilder:AlertDialog.Builder
+    //프로필 이미지  편집 용 dialog 와 그 builder
+    private lateinit var editProfileImageDialog: AlertDialog
+    private lateinit var editProfileImageDialogBuilder:AlertDialog.Builder
+
 
     //기기 화면 사이즈를 가져오는 클래스  1-1
-    lateinit var getWindowSize: GetWindowSize
+    private lateinit var getWindowSize: GetWindowSize
+
 
     //키보드 관련 처리 1-2
-    lateinit var keyboardutil:KeyboardVisibilityUtils
+    private lateinit var keyboardutil:KeyboardVisibilityUtils
 
+
+    //프로필 이미지 변경 여부 체크
+    //1=> 프로필 없애기 2=> 새이미지로 변경 3=> 그냥 그대로
+    private  var profileImgChangeCheck:Int =3
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -68,9 +83,13 @@ class UpdateUserInFoDialog(
         view.edittxt_for_nickname.setText(nickname)
         view.edittxt_for_nickname.setSelection(nickname.length)//cursor 맨뒤로
 
+        //profile_url 이 있을 경우  -> glide 에 넣어줌.
         if(!(profile_url.isNullOrEmpty())){
+
+            Logger.v("프로필 url 이있다. ")
             Glide.with(requireActivity()).load(profile_url).into(view.profile_image)
         }
+
 
 
         return view
@@ -103,34 +122,11 @@ class UpdateUserInFoDialog(
                         packagename = requireActivity().packageName,
                         activity = requireActivity())
                     ){
+
                         Logger.v("모든 권한 허용 됨")
 
-
-                        val inflater:LayoutInflater= requireActivity().getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
-                        val view=inflater.inflate(R.layout.custom_profile_img_edit_dialog,null)
-                        editProfileImageDialogBuilder=AlertDialog.Builder(requireActivity())
-                        editProfileImageDialogBuilder.setView(view)
-                        editProfileImageDialogBuilder.setCancelable(false)
-
-                        editProfileImageDialog=editProfileImageDialogBuilder.create()
-                        editProfileImageDialog.show()
-
-
-                        view.txt_no_img.setOnClickListener {
-
-                           Toast.makeText(requireActivity(),"프로필 없음",Toast.LENGTH_SHORT).show()
-
-                        }
-
-                        view.txt_for_goto_gallery.setOnClickListener{
-
-                            Toast.makeText(requireActivity(),"갤러리 가기기",Toast.LENGTH_SHORT).show()
-                        }
-
-                       view.txt_for_cancel_dialog.setOnClickListener {
-                            editProfileImageDialog.dismiss()
-                        }
-
+                        //모든 권한 허용이므로 ->  프로필 이미지 다이얼로그 보이기 진행
+                        showProfileImgEditDialog()
 
                     }else{//아직 허용 안된 권한이 있다.
                         Logger.v("아직 허용 안된 권한이 존재")
@@ -143,7 +139,8 @@ class UpdateUserInFoDialog(
                 //현재 버전의 min sdk 23이므로 상관 없지만, 혹시 몰라 else 넣어둠.
 
                 Logger.v("모든 권한 허용됨")
-
+                //모든 권한 허용이므로 ->  프로필 이미지 다이얼로그 보이기 진행
+                showProfileImgEditDialog()
             }
 
         })//이미지 편집 2-1
@@ -155,7 +152,98 @@ class UpdateUserInFoDialog(
     }
 
 
-     //클릭 이벤트 모음
+
+    //프로필  이미지 편집 다이얼로그를  띄어준다.
+    private fun showProfileImgEditDialog(){
+
+        //프로필 이미지 편집 다이얼로그 설정
+        val inflater:LayoutInflater= requireActivity().getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val editProfileImgDialogView=inflater.inflate(R.layout.custom_profile_img_edit_dialog,null)
+        editProfileImageDialogBuilder=AlertDialog.Builder(requireActivity())
+        editProfileImageDialogBuilder.setView(editProfileImgDialogView)
+        editProfileImageDialogBuilder.setCancelable(false)
+
+        editProfileImageDialog=editProfileImageDialogBuilder.create()
+        editProfileImageDialog.show()
+
+
+
+        //프로필 없애기 눌림
+        editProfileImgDialogView.txt_no_img.setOnClickListener {
+            Logger.v("프로필 없애기 눌림")
+
+            profile_image.setImageResource(R.drawable.profile_img)
+
+            //1=> 프로필 없애기 2=> 새이미지로 변경 3=> 그냥 그대로
+            profileImgChangeCheck=1
+
+            editProfileImageDialog.dismiss()
+        }
+
+
+
+        //갤러리 가기 눌림
+        editProfileImgDialogView.txt_for_goto_gallery.setOnClickListener{
+
+
+            Logger.v("갤러리 가기 클릭됨")
+
+
+            TedImagePicker.with(requireActivity())
+                .mediaType(MediaType.IMAGE)
+                .showCameraTile(false)
+                .title(R.string.string_for_select_profile_image)
+                .backButton(R.drawable.arrow_icon_for_back)
+                .startAnimation(R.anim.fadein,R.anim.fadeout)
+                .start { uri ->
+
+                    //1=> 프로필 없애기 2=> 새이미지로 변경 3=> 그냥 그대로
+                    profileImgChangeCheck=2
+
+                    Toast.makeText(requireActivity(),"",Toast.LENGTH_SHORT).show()
+
+                    profile_image.setImageURI(uri)
+                    editProfileImageDialog.dismiss()
+                }
+
+
+        }
+
+        //취소 버튼 눌림
+        editProfileImgDialogView.txt_for_cancel_dialog.setOnClickListener {
+
+            //1=> 프로필 없애기 2=> 새이미지로 변경 3=> 그냥 그대로
+            profileImgChangeCheck=3
+
+            Logger.v("취소 버튼 클릭됨")
+            editProfileImageDialog.dismiss()
+        }
+
+
+
+        //원 상태로 돌리기
+        editProfileImgDialogView.tv_return_original_profile.setOnClickListener {
+
+            Logger.v("원상태로 돌리기 클릭됨")
+
+            //1=> 프로필 없애기 2=> 새이미지로 변경 3=> 그냥 그대로
+            profileImgChangeCheck=3
+
+            Glide.with(requireActivity())
+                .load(profile_url)
+                .placeholder(R.drawable.profile_img)
+                .into(profile_image)
+
+            editProfileImageDialog.dismiss()
+        }
+
+
+    }//showProfileImgEditDialog () 끝
+
+
+
+
+    //클릭 이벤트 모음
      private val clickListener=View.OnClickListener {
 
          when(it){
@@ -182,12 +270,15 @@ class UpdateUserInFoDialog(
      }
 
 
+
     //constaring 그룹으로 묶인 뷰들  클릭 할때 사용
     private fun Group.setAllOnClickListener(listener: View.OnClickListener?) {
         referencedIds.forEach { id ->
             rootView.findViewById<View>(id).setOnClickListener(listener)
         }
     }
+
+
 
     override fun onResume() {
         super.onResume()
